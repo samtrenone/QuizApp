2a89e9c439d84e79cec1bd2763efc06cbca43026
Samtrenone <samtrenone@gmail.com>
2021-11-28 12:25:15 +0100

    Edit - style modal

--- js/modal.js
+++ js/modal.js
@@ -21,27 +21,29 @@ function saveShowParams(){
 
 //iif to create the modal box
 (function createModal(){
-    //this.root.innerHTML
-    // <div class="modal">
-    //     <div class="modal-overlay"></div>
-    //     <div class="modal-content"></div>
-    // </div>
     let newDivModal = document.createElement('div');
     newDivModal.classList.add('modal');
 
     let newDivModalOverlay = document.createElement('div');
     newDivModalOverlay.classList.add('modal-overlay');
-    newDivModalOverlay.addEventListener('click', toggleModal);
 
     let newDivModalContent = document.createElement('div');
     newDivModalContent.classList.add('modal-content');
 
     let newFormModal = document.createElement('form');
+    newFormModal.classList.add('modal-content-form');
 
+    let newFieldSet;
+    let newLegend;
     let newLabelCheck;
     let newInputCheck;
 
     //sources
+    newFieldSet = document.createElement('fieldset');
+    newLegend = document.createElement('legend');
+    newLegend.textContent = 'Sources:'
+    newFieldSet.appendChild(newLegend);
+
     showParams.sources.forEach((source,index) =>{
         newInputCheck = document.createElement('input');
         newInputCheck.type = 'checkbox';
@@ -52,10 +54,17 @@ function saveShowParams(){
         newLabelCheck = document.createElement('label');
         newLabelCheck.htmlFor = 'chkShowSource' + index;
         newLabelCheck.appendChild(newInputCheck);
-        newLabelCheck.appendChild(document.createTextNode('Show source: ' + dataSources[index].title));
+        newLabelCheck.appendChild(document.createTextNode(dataSources[index].title));
     
-        newFormModal.appendChild(newLabelCheck);
+        newFieldSet.appendChild(newLabelCheck);
     });
+    newFormModal.appendChild(newFieldSet);
+    
+
+    newFieldSet = document.createElement('fieldset');
+    newLegend = document.createElement('legend');
+    newLegend.textContent = 'Questions:'
+    newFieldSet.appendChild(newLegend);
 
     //wrong questions
     newInputCheck = document.createElement('input');
@@ -67,10 +76,10 @@ function saveShowParams(){
     newLabelCheck = document.createElement('label');
     newLabelCheck.htmlFor = 'chkShowWrong';
     newLabelCheck.appendChild(newInputCheck);
-    newLabelCheck.appendChild(document.createTextNode('Show wrong answered questions'));
+    newLabelCheck.appendChild(document.createTextNode('Wrong'));
 
-    newFormModal.appendChild(newLabelCheck);
-
+    newFieldSet.appendChild(newLabelCheck);
+    
     //right questions
     newInputCheck = document.createElement('input');
     newInputCheck.type = 'checkbox';
@@ -81,9 +90,9 @@ function saveShowParams(){
     newLabelCheck = document.createElement('label');
     newLabelCheck.htmlFor = 'chkShowRight';
     newLabelCheck.appendChild(newInputCheck);
-    newLabelCheck.appendChild(document.createTextNode('Show right answered questions'));
+    newLabelCheck.appendChild(document.createTextNode('Right'));
 
-    newFormModal.appendChild(newLabelCheck);
+    newFieldSet.appendChild(newLabelCheck);
 
     //unanswered questions
     newInputCheck = document.createElement('input');
@@ -95,11 +104,18 @@ function saveShowParams(){
     newLabelCheck = document.createElement('label');
     newLabelCheck.htmlFor = 'chkShowUnasnwered';
     newLabelCheck.appendChild(newInputCheck);
-    newLabelCheck.appendChild(document.createTextNode('Show unanswered questions'));
+    newLabelCheck.appendChild(document.createTextNode('Unanswered'));
 
-    newFormModal.appendChild(newLabelCheck);
+    newFieldSet.appendChild(newLabelCheck);
+
+    newFormModal.appendChild(newFieldSet);
 
     //domains
+    newFieldSet = document.createElement('fieldset');
+    newLegend = document.createElement('legend');
+    newLegend.textContent = 'Domains:'
+    newFieldSet.appendChild(newLegend);
+
     showParams.domains.forEach((domain,index) =>{
         newInputCheck = document.createElement('input');
         newInputCheck.type = 'checkbox';
@@ -110,10 +126,11 @@ function saveShowParams(){
         newLabelCheck = document.createElement('label');
         newLabelCheck.htmlFor = 'chkShowDomain' + index;
         newLabelCheck.appendChild(newInputCheck);
-        newLabelCheck.appendChild(document.createTextNode('Show Domain: ' + dataDomains[index].title));
+        newLabelCheck.appendChild(document.createTextNode(dataDomains[index].title));
     
-        newFormModal.appendChild(newLabelCheck);
+        newFieldSet.appendChild(newLabelCheck);
     });
+    newFormModal.appendChild(newFieldSet);
 
     //submit button
     let newSubmit = document.createElement('input');
