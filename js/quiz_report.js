f6247ccd82e1cd754d5f24c10797f0e8e405aea4
Samtrenone <samtrenone@gmail.com>
2021-12-11 11:46:20 +0100

    Fix - same answers order in quiz and report

--- js/quiz_report.js
+++ js/quiz_report.js
@@ -1,7 +1,7 @@
-function renderQuizReport(){
-    let newDivTotals = document.createElement('div');
-    newDivTotals.classList.add('quiz__report-totals');
-    let quizReportContent = `<p>Total time: <span class="time"></span></p>
+function renderQuizReport() {
+  let newDivTotals = document.createElement("div");
+  newDivTotals.classList.add("quiz__report-totals");
+  let quizReportContent = `<p>Total time: <span class="time"></span></p>
     <div class="quiz__report-results">
     <p>
         <span class="quiz__report-answered">Total answered: ${quiz.getTotalAnswered()} of ${quiz.getTotalQuestions()}</span>
@@ -10,48 +10,63 @@ function renderQuizReport(){
     </p>
     <br/>`;
 
-    for(let i=0 ; i<dataDomains.length ; i++){
-        quizReportContent += `<p>
-        <span class="quiz__report-answered">${dataDomains[i].title}: ${quiz.getTotalAnsweredByDomain(i)} of ${quiz.getTotalQuestionsByDomain(i)}</span>
-        <span class="quiz__report-right">${quiz.getTotalRightByDomain(i)} / ${quiz.getPercentageRightByDomain(i)}%</span>
-        <span class="quiz__report-wrong">${quiz.getTotalWrongByDomain(i)} / ${quiz.getPercentageWrongByDomain(i)}%</span>
+  for (let i = 0; i < dataDomains.length; i++) {
+    quizReportContent += `<p>
+        <span class="quiz__report-answered">${
+          dataDomains[i].title
+        }: ${quiz.getTotalAnsweredByDomain(
+      i
+    )} of ${quiz.getTotalQuestionsByDomain(i)}</span>
+        <span class="quiz__report-right">${quiz.getTotalRightByDomain(
+          i
+        )} / ${quiz.getPercentageRightByDomain(i)}%</span>
+        <span class="quiz__report-wrong">${quiz.getTotalWrongByDomain(
+          i
+        )} / ${quiz.getPercentageWrongByDomain(i)}%</span>
     </p>`;
-    }
+  }
 
-    newDivTotals.innerHTML = quizReportContent;
+  newDivTotals.innerHTML = quizReportContent;
 
-    quiz.root.appendChild(newDivTotals);
-    
-    renderTimer(quiz.totalSeconds, newDivTotals.querySelector('.time'));
-    renderQuizReportQuestions()
+  quiz.root.appendChild(newDivTotals);
+
+  renderTimer(quiz.totalSeconds, newDivTotals.querySelector(".time"));
+  renderQuizReportQuestions();
 }
 
 function filterQuestionsReport(e) {
+  if (e.target.matches("input")) {
+    let questions = Array.from(
+      document.getElementsByClassName("quiz__report-question")
+    );
 
-    if (e.target.matches('input')) {
-        let questions = Array.from(document.getElementsByClassName('quiz__report-question'));
+    questions.forEach((el) => el.classList.remove("hide"));
 
-        questions.forEach(el => el.classList.remove('hide'));
-
-        switch(e.target.value){
-            case 'marked':
-                questions.filter(el => el.dataset.marked !== 'true').forEach(el => el.classList.add('hide'));
-                break;
-            case 'right':
-                questions.filter(el => el.dataset.result !== 'right').forEach(el => el.classList.add('hide'));
-                break;
-            case 'wrong':
-                questions.filter(el => el.dataset.result !== 'wrong').forEach(el => el.classList.add('hide'));
-                break;
-        }
+    switch (e.target.value) {
+      case "marked":
+        questions
+          .filter((el) => el.dataset.marked !== "true")
+          .forEach((el) => el.classList.add("hide"));
+        break;
+      case "right":
+        questions
+          .filter((el) => el.dataset.result !== "right")
+          .forEach((el) => el.classList.add("hide"));
+        break;
+      case "wrong":
+        questions
+          .filter((el) => el.dataset.result !== "wrong")
+          .forEach((el) => el.classList.add("hide"));
+        break;
     }
+  }
 }
 
 function renderQuizReportQuestions() {
-    let newDivFilter = document.createElement('div');
-    newDivFilter.classList.add('quiz__report-filter');
-    newDivFilter.addEventListener('click',filterQuestionsReport);
-    newDivFilter.innerHTML = `
+  let newDivFilter = document.createElement("div");
+  newDivFilter.classList.add("quiz__report-filter");
+  newDivFilter.addEventListener("click", filterQuestionsReport);
+  newDivFilter.innerHTML = `
     <label for="filterAll">
         <input type="radio" name="filter" id="filterAll" value="all" checked></input>
         All
@@ -70,57 +85,68 @@ function renderQuizReportQuestions() {
     </label>
     `;
 
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('quiz__report-index');
-    
-    let newUl = document.createElement('ul');
-    quiz.questions.forEach((question,index) => {
-        let dataQuestion = getDataQuestion(question.id);
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("quiz__report-index");
 
-        let newLi = document.createElement('li');
-        newLi.classList.add('quiz__report-question');
-        if (question.marked) {
-            newLi.classList.add('quiz__report-question--marked');
-            newLi.setAttribute('data-marked',true);
-        }
-        if (question.result == 'right') {
-            newLi.classList.add('quiz__report-question--right');
-            newLi.setAttribute('data-result','right');
-        }
-        if (question.result == 'wrong') {
-            newLi.classList.add('quiz__report-question--wrong');
-            newLi.setAttribute('data-result','wrong');
-        }
+  let newUl = document.createElement("ul");
+  quiz.questions.forEach((question, index) => {
+    let dataQuestion = getDataQuestion(question.id);
 
-        newLi.addEventListener('click', function(){
-            this.querySelector('.quiz__report-panel').classList.toggle('quiz__report-panel--show');
-        });
+    let newLi = document.createElement("li");
+    newLi.classList.add("quiz__report-question");
+    if (question.marked) {
+      newLi.classList.add("quiz__report-question--marked");
+      newLi.setAttribute("data-marked", true);
+    }
+    if (question.result == "right") {
+      newLi.classList.add("quiz__report-question--right");
+      newLi.setAttribute("data-result", "right");
+    }
+    if (question.result == "wrong") {
+      newLi.classList.add("quiz__report-question--wrong");
+      newLi.setAttribute("data-result", "wrong");
+    }
 
-        let newUlAnswers = document.createElement('ul');
-        dataQuestion.answers.forEach(answer => {
-            let newLiAnswer = document.createElement('li');
-            if(answer.correct){
-                newLiAnswer.classList.add('quiz__report-answer--right');
-            }else if(answer.id == question.selectedId && question.result == 'wrong'){
-                newLiAnswer.classList.add('quiz__report-answer--wrong');
-            }else{
-                newLiAnswer.classList.add('quiz__report-answer');
-            }
-            newLiAnswer.textContent = answer.answer;
-            newUlAnswers.appendChild(newLiAnswer)
-        });
+    newLi.addEventListener("click", function () {
+      this.querySelector(".quiz__report-panel").classList.toggle(
+        "quiz__report-panel--show"
+      );
+    });
 
-        newLi.innerHTML = `<i class="fa fa-bookmark mark"></i><p class="quiz__report-question-index">${index+1}</p>
+    let newUlAnswers = document.createElement("ul");
+
+    question.answers.forEach((answer) => {
+      let dataAnswer = getDataQuestionAnswer(dataQuestion, answer.id);
+      let newLiAnswer = document.createElement("li");
+      if (dataAnswer.correct) {
+        newLiAnswer.classList.add("quiz__report-answer--right");
+      } else if (
+        answer.id == question.selectedId &&
+        question.result == "wrong"
+      ) {
+        newLiAnswer.classList.add("quiz__report-answer--wrong");
+      } else {
+        newLiAnswer.classList.add("quiz__report-answer");
+      }
+      newLiAnswer.textContent = dataAnswer.answer;
+      newUlAnswers.appendChild(newLiAnswer);
+    });
+
+    newLi.innerHTML = `<i class="fa fa-bookmark mark"></i><p class="quiz__report-question-index">${
+      index + 1
+    }</p>
         <p>${dataQuestion.question}</p>
         <div class="quiz__report-panel">
         ${newUlAnswers.outerHTML}
         <p class="quiz__report-explanation">${dataQuestion.explanation}</p>
-        <p class="quiz__report-reference">${dataQuestion.domain}<br/>${dataQuestion.source}</p>
+        <p class="quiz__report-reference">${dataQuestion.domain}<br/>${
+      dataQuestion.source
+    }</p>
         </div>`;
-        newUl.appendChild(newLi);
-    });
+    newUl.appendChild(newLi);
+  });
 
-    newDiv.appendChild(newDivFilter);
-    newDiv.appendChild(newUl);
-    quiz.root.appendChild(newDiv);
+  newDiv.appendChild(newDivFilter);
+  newDiv.appendChild(newUl);
+  quiz.root.appendChild(newDiv);
 }
